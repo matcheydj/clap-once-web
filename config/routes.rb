d176a7a265da697a1ce329ab356609376758fe20
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-07-29 12:51:10 -0700

    adds route to user dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/dashboard', to: 'users#dashboard'
+
+  root 'users#routing'
 end
