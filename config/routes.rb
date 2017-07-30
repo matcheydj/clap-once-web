b18632cdbcea5eac4edb01681da88910d0924e0d
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-07-30 08:48:45 -0700

    adds incidents index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/dashboard', to: 'users#dashboard'
+  get '/incidents', to: 'incidents#index'
+
 
   root 'users#routing'
 end
