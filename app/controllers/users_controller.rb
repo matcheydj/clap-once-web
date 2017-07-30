9cf56faed459de6180b6dbb9b64a84524b114a3e
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-07-30 06:05:15 -0700

    adds a method for retreiving and parsing API information and sending it to the dashboard view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def routing
     if session[:id]
       redirect '/dashboard'
@@ -7,7 +6,9 @@ class UsersController < ApplicationController
   end
 
   def dashboard
-
+    # response = ClaponceApiAdapter.index("<route to api>", { user: { id: session[:id] } }  )
+    # @incidents = JSON.parse(response.body)['incidents']
+    @incidents = [{ id: 2, type: "flood", description: "Flood in the SF", victims: [{ id: 2, name: "Jasper"},{id: 1, name: "Steve"}] }, { id: 1, type: "fire", description: "Fire in the San Jose", victims: [{id: 1, name: "Steve"}] }].as_json
   end
 
 end
