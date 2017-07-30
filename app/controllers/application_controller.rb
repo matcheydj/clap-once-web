dd12538a60cdf3d358816548a8bf0643a47b2aba
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-07-30 08:48:12 -0700

    adds before action to monitor page location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :page_location
+
+  private
+
+  def page_location
+    session[:page] = nil
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
   def routing
-    if session[:id]
-      redirect '/dashboard'
-    end
+    redirect_to '/dashboard'
   end
 
   def dashboard
+    session[:page] = "dashboard"
     # response = ClaponceApiAdapter.index("<route to api>", { user: { id: session[:id] } }  )
     # @incidents = JSON.parse(response.body)['incidents']
     @incidents = [{ id: 2, type: "flood", description: "Flood in the SF", victims: [{ id: 2, name: "Jasper"},{id: 1, name: "Steve"}] }, { id: 1, type: "fire", description: "Fire in the San Jose", victims: [{id: 1, name: "Steve"}] }].as_json
