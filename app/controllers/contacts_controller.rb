0fe812277ce2acb82f070b47a3c7d2321bf7ad2b
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-07-30 11:40:02 -0700

    Last Commit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,7 +4,7 @@ class ContactsController < ApplicationController
   def index
     # response = ClaponceApiAdapter.index("<route to api>", { user: { id: session[:id] } }  )
     # @incidents = JSON.parse(response.body)['incidents']
-    @contacts = [{ id: 2, name: "Jasper", phoneNumber: "4504504444"}, {id: 1, name: "Steve",  phoneNumber: "4504507444"}].as_json
+    @contacts = [{ id: 1, name: "Chinmay Banker", phoneNumber: "415-555-5555"},{id: 2, name: "Joanna Joseph", phoneNumber: "415-555-5550"},{ id: 3, name: "Roger Li", phoneNumber: "415-555-5552"}].as_json
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,9 @@ class UsersController < ApplicationController
     if response.code == 1
       @incidents = JSON.parse(response.body)['incidents']
     else
-      @incidents = [{ id: 3, name: "Flash Flood", description: "Flood hits Silver Creek Area", affected_users: [{ id: 1, name: "Chinmay Banker", phoneNumer: "415-555-5555"}, {id: 2, name: "Joanna Joseph", phoneNumer: "415-555-5550"}] },
-        { id: 2, name: "Earthquake", description: "Massive Earthquake hits SF", affected_users: [{ id: 1, name: "Chinmay Banker", phoneNumer: "415-555-5555"}, {id: 2, name: "Joanna Joseph", phoneNumer: "415-555-5550"}] },
-      { id: 1, name: "Fire", description: "4-Alarm fire erupts in Oakland Hills", affected_users: [{ id: 1, name: "Chinmay Banker", phoneNumer: "415-555-5555"},{id: 2, name: "Joanna Joseph", phoneNumer: "415-555-5550"},{ id: 3, name: "Roger Li", phoneNumer: "415-555-5552"}]} ].as_json
+      @incidents = [{ id: 3, name: "Flash Flood", description: "Flood hits Silver Creek Area", affected_users: [{ id: 1, name: "Chinmay Banker"}] },
+        { id: 2, name: "Earthquake", description: "Massive Earthquake hits SF", affected_users: [{ id: 1, name: "Chinmay Banker"}, {id: 2, name: "Joanna Joseph"}, { id: 3, name: "Roger Li"}] },
+      { id: 1, name: "Fire", description: "4-Alarm fire erupts in Oakland Hills", affected_users: [{ id: 1, name: "Chinmay Banker"},{id: 2, name: "Joanna Joseph"},{ id: 3, name: "Roger Li"}]} ].as_json
     end
   end
 
